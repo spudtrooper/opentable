4f7620ccdc9ce621f444c9c01837b0d076a11ec0
Jeff Palm <jeff@jeffpalm.com>
2022-09-20 06:33:27 -0400

    update Tue Sep 20 06:33:27 EDT 2022

--- scripts/export.sh
+++ scripts/export.sh
@@ -5,5 +5,10 @@
 set -e
 
 mkdir -p data
-# mongoexport --db=opentable --collection=restaurants --type=json > data/restaurants.json
+mkdir -p data/scripts
+
+mongoexport --db=opentable --collection=restaurants --type=json > data/restaurants.json
+
+cat playgrounds/sort-by-price.mongodb | sed 's#use(#//use(#g' > data/scripts/sort-by-price.js
+mongo mongodb://localhost:27017/seats data/scripts/sort-by-price.js
 mongoexport --db=opentable --collection=sortedByPrice --type=json --fields=restaurantName,restaurantWebsite,title,price > data/sortedByPrice.json
